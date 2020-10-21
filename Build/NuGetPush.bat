9c09a436c8640bb75e1ebd15bc35b441635ef7a1
David Small <dave@dsmall.co.uk>
2020-10-21 21:44:11 +0100

    feat: Release beta verson with restore packages and build solution instead of project.

--- Build/NuGetPush.bat
+++ Build/NuGetPush.bat
@@ -1,14 +1,16 @@
 @echo off
-SET packageVersion=2.0.0-alpha03
+SET packageVersion=2.0.0-beta01
 
 SET configuration=Release
-SET id="Springboard365.Tools.CommandLine.Core";
-SET author="Springboard 365 Ltd";
-SET repo="https://github.com/SpringBoard365/Springboard365.Tools.CommandLine.Core";
-SET description="The core helper project to allow parameters to be passed in and verified on a console application.";
-SET tags="CommandLine Springboard365ToolHelper";
+SET id=Springboard365.Tools.CommandLine.Core
+SET author="Springboard 365 Ltd"
+SET repo="https://github.com/SpringBoard365/Springboard365.Tools.CommandLine.Core"
+SET description="The core helper project to allow parameters to be passed in and verified on a console application."
+SET tags="CommandLine Springboard365ToolHelper"
 
-dotnet build ../src/CommandLine.Core.csproj -c  %configuration% -p:Version=%packageVersion% -f net462 --nologo
+dotnet restore ../%id%.sln
+
+dotnet build ../%id%.sln -c  %configuration% -p:Version=%packageVersion% -f net462 --nologo
 
 pause
 
